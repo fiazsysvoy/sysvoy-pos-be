4b8e039a220369ec2a490066b0712b3b8990eceb
Raven Moiz <abmoiz389@gmail.com>
2025-12-29 13:50:17 +0500

    cors added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import routes from "./routes.js";
+import cors from 'cors'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
 app.use(express.json());
 app.use("/api", routes);
 
